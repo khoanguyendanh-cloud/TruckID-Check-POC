28ecb54720c69ed4061c07a5caa5607bbcd1227e
khoanguyendanh-cloud <khoa.nguyendanh@spxexpress.com>
2026-08-15 13:38:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.TRUCK_CHECK_CONFIG = Object.freeze({
   // Example:
   // https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
   // ==========================================================
-  appsScriptUrl: "https://script.google.com/a/macros/spxexpress.com/s/AKfycbylXiEy1l8gAB3Ym0VGYBtyOxCURXuXn4N7CZGXP3pL6DKVPwNzZy2yEnb6jo87dZdz/exec?action=ping",
+  appsScriptUrl: "https://script.google.com/a/macros/spxexpress.com/s/AKfycbylXiEy1l8gAB3Ym0VGYBtyOxCURXuXn4N7CZGXP3pL6DKVPwNzZy2yEnb6jo87dZdz/exec",
   
   modelLocalUrl: "./models/cct_xs_v2_global.onnx?v=2",
 
